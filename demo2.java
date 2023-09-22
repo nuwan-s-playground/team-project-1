dc6b5b9e01e7c9cfbc7e9c84adb56410304694d7
IsuruWeerakkodi <isuruweerakkodi24@gmail.com>
2023-09-22 13:13:40 +0530

    add algorithm 2

---
+++ demo2.java
@@ -0,0 +1,11 @@
+public class demo2 {
+    public static void main(String[] args) {
+        
+            for (int i = 0; i < 5; i++) {
+            System.out.print("* ".repeat(5-i));
+            System.out.println();
+            
+            }
+        
+    }
+}
